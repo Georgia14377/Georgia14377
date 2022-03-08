23fee1a80745ee35013032ad44a5f140e9674fa4
105328501 <105328501@L815WD150038.edu.cbe.ab.ca>
2022-03-08 15:16:07 -0700

    As1

---
+++ Credit3CS10/src/Assignments/Assignment1.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class Assignment1 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("From: Mr. Abdalla");
+		System.out.println("Adress: Acer Computer, Crescent Heights");
+		System.out.println("Date:March 8th,2022");
+		System.out.println("To:Georgia Mollet");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+	}
+
+}
