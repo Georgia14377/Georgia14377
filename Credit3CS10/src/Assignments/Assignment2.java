a1e6433e07fb4a3c73e576664fc3d3f4c8751756
105328501 <105328501@L815WD150038.edu.cbe.ab.ca>
2022-03-14 13:15:47 -0600

    ok

---
+++ Credit3CS10/src/Assignments/Assignment2.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Assignment2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" -----      |       |\r\n"
+				+ "|     |     |\\     /| \r\n"
+				+ "|           | \\___/ | \r\n"
+				+ "|  ----     |       | \r\n"
+				+ "|_____|     |       |");
+		
+	}	
+}
