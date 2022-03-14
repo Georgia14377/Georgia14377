9d1eb98a927ee26be96a0c0e554d0563ce7ac1ea
105328501 <105328501@L815WD150038.edu.cbe.ab.ca>
2022-03-14 13:24:39 -0600

    ch

---
+++ Credit3CS10/src/Assignments/Assignment3.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		System.out.println("B   I    N    G    O\r\n"
+				+ "14  29   32   58   74\r\n"
+				+ "3   22   43   47   61\r\n"
+				+ "8   18  FREE  53   65\r\n"
+				+ "15  16   45   59   71\r\n"
+				+ "6   26   28   60   63");
+	}
+
+}
