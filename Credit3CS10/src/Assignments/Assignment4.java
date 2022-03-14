c0eb6294fe76a61c8755c640a4063bef59eebe6d
105328501 <105328501@L815WD150038.edu.cbe.ab.ca>
2022-03-14 13:48:51 -0600

    working on it

---
+++ Credit3CS10/src/Assignments/Assignment4.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		int diameter= 10;
+		int diacost= diameter * 0.05;
+	
+ 
+		System.out.println("diameter of pizza is "+ diameter
+				+ diacost + " dollars plus the set $0.75 + $1.00 ");
+				 
+		
+				
+
+	}
+
+}
