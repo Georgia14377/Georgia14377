d7a3f48d0257ee52a709e7ef224cc9c5058e6083
105328501 <105328501@L815WD150038.edu.cbe.ab.ca>
2022-03-18 11:07:43 -0600

    c4

---
+++ Credit4cs10/src/Assignments/assi5.java
@@ -0,0 +1,28 @@
+/*
+
+Program: assi5.java          Date: March 18/'22
+
+
+Author: Georgia Mollet
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+
+package Assignments;
+
+public class assi5 {
+
+	public static void main(String[] args)
+	
+	{
+		/*79 + 3 * (4 + 82 –68) – 7 +19
+		(179 +21 +10) / 7 + 181
+		10389 * 56 * 11 + 2246 
+      */
+
+	}
+
+}
