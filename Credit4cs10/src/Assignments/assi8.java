5a95e9956c975858dce3ab422a8dbf600ab76c89
105328501 <105328501@L815WD220017.edu.cbe.ab.ca>
2022-03-30 12:48:50 -0600

    ASsig8+

--- Credit4cs10/src/Assignments/assi8.java
+++ Credit4cs10/src/Assignments/assi8.java
@@ -1,5 +1,15 @@
 package Assignments;
+/*
 
+Program: assi7.java          Date: march 30th
+
+
+Author: Georgia Mollet
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
 import java.util.Scanner;
 
 public class assi8 {
@@ -8,11 +18,23 @@ public class assi8 {
 	{
 		Scanner userinput = new Scanner(System.in);
 		
-		System.out.println("Enter a two digit nuber:"); 
+		System.out.println("Enter a two digit number:"); 
 		int number = userinput.nextInt();
 
 		int tens = number / 10;
 		System.out.println("Tens-place:" + tens); 
+		int ones = (number - (tens *10));
+		System.out.println("Ones-place:" + ones);
+		
 	}
 
 }
+/* screen dump
+Enter a two digit nuber:
+98
+Tens-place:9
+Ones-place:8
+
+
+
+*/
